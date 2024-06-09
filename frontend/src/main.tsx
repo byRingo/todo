dbe978700da4788204de9d16ea73ec67b03305d6
Victor <vav0701@mail.ru>
2024-06-09 14:42:24 +0500

    react strict mode

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,10 +6,10 @@ import store from "./store";
 import GlobalStyles from "./styles/global.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <>
+  <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
     <GlobalStyles></GlobalStyles>
-  </>,
+  </React.StrictMode>,
 );
